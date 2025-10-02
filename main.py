63c592a8ba34ce205a66173f24440361061bb33b
Marina Barramus <marinabarramus@MacBook-Air-Marina.local>
2025-10-02 20:42:05 +0300

    Updates

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import os
 import logging
 from dotenv import load_dotenv
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
